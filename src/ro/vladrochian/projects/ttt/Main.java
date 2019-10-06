f52d7007ce18520e3156a5725d2a9bc5406625eb
Vlad Rochian <vlad.rochian@gmail.com>
2019-10-06 17:47:48 +0300

    Add user interaction for game mode

--- src/ro/vladrochian/projects/ttt/Main.java
+++ src/ro/vladrochian/projects/ttt/Main.java
@@ -4,10 +4,16 @@ import ro.vladrochian.projects.ttt.algorithm.AStarAlgorithm;
 import ro.vladrochian.projects.ttt.algorithm.Algorithm;
 import ro.vladrochian.projects.ttt.game.Game;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    int mode = args.length > 0 ? Integer.parseInt(args[0]) : getMode();
+    if (mode < 0 || mode > 3) {
+      mode = 0;
+    }
     Algorithm botAlgorithm = new AStarAlgorithm();
-    boolean[] players = {true, false};
+    boolean[] players = {(mode & 1) == 1, (mode >> 1) == 1};
     Game game = new Game(players, botAlgorithm);
     game.play();
     int winner = game.winner();
@@ -18,5 +24,12 @@ public class Main {
     } else {
       System.out.println("Computer wins!");
     }
+  }
+
+  private static int getMode() {
+    System.out.println("Game mode: 0 = computer to computer, 1 = join as P1, 2 = join as P2, 3 = two player mode");
+    System.out.println("Enter game mode:");
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextInt();
   }
 }
